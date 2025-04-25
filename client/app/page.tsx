96ba29bcdaf2bdb1b69d4e63db3ca0b981f048db
andrew-dev-p <popov.andrew.dev@gmail.com>
2025-04-25 17:45:51 +0300

    Redirect from root page if authed

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { useAuthStore } from "@/stores/auth-store";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const user = useAuthStore((state) => state.user);
+  const route = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      route.push("/feed");
+    }
+  }, [route, user]);
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-4">
       <div className="w-full max-w-md space-y-8 rounded-lg border p-8 shadow-sm">
